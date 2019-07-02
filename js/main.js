c5f06c638798381c1ed76eb915fda873552befdd
wyx8267 <369264448@qq.com>
2019-07-02 16:30:01 +0800

    css finished

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.getElementById('portfolioAll').onclick=function () {
+    portfolioBar.className='bar state-1';
+}
+document.getElementById('portfolioFramework').onclick=function () {
+    portfolioBar.className='bar state-2';
+}
+document.getElementById('portfolioVallina').onclick=function () {
+    portfolioBar.className='bar state-3';
+}
